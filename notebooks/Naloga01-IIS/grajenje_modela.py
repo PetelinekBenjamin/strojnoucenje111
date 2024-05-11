9cd86933ac2f38e8c88e02431c79cab69dcde484
PetelinekBenjamin <59640546+PetelinekBenjamin@users.noreply.github.com>
2024-05-11 12:23:54 +0200

    Update grajenje_modela.py

--- notebooks/Naloga01-IIS/grajenje_modela.py
+++ notebooks/Naloga01-IIS/grajenje_modela.py
@@ -11,26 +11,28 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
 import mlflow
 import os
+import tf2onnx
+
 
 # Nastavitev sledenja MLflow
-# mlflow.set_tracking_uri('https://dagshub.com/PetelinekBenjamin/strojnoucenje111.mlflow')
-mlflow.set_tracking_uri("https://dagshub.com/ZanPovseGit/inteligentniSistem.mlflow")
+mlflow.set_tracking_uri('https://dagshub.com/PetelinekBenjamin/strojnoucenje111.mlflow')
+# mlflow.set_tracking_uri("https://dagshub.com/ZanPovseGit/inteligentniSistem.mlflow")
 
 # Nastavitev uporabniškega imena in gesla
-# os.environ["MLFLOW_TRACKING_USERNAME"] = "PetelinekBenjamin"
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = "30663408e580bdb3f66e074627577a040f36b5ff"
-os.environ["MLFLOW_TRACKING_USERNAME"] = "ZanPovseGit"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "bdf091cc3f58df2c8346bb8ce616545e0e40b351"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "PetelinekBenjamin"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "30663408e580bdb3f66e074627577a040f36b5ff"
+# os.environ["MLFLOW_TRACKING_USERNAME"] = "ZanPovseGit"
+# os.environ["MLFLOW_TRACKING_PASSWORD"] = "bdf091cc3f58df2c8346bb8ce616545e0e40b351"
 
 def pripravi_podatke_za_ucenje(vrednosti, okno_velikost):
     X, y = [], []
     for i in range(len(vrednosti) - okno_velikost):
-       X.append(vrednosti[i:i+okno_velikost, :])
-       y.append(vrednosti[i+okno_velikost, -1])
+        X.append(vrednosti[i:i+okno_velikost, :])
+        y.append(vrednosti[i+okno_velikost, -1])
     return np.array(X), np.array(y)
 
 # Začetek MLflow teka
-with mlflow.start_run(run_name="MyModelTraining"):
+with mlflow.start_run(run_name="MyModelTrainingUcenje"):
 
 
 
@@ -38,10 +40,6 @@ with mlflow.start_run(run_name="MyModelTraining"):
     pot_do_datoteke = r'data/processed/train_prod.csv'
     df = pd.read_csv(pot_do_datoteke, parse_dates=['last_update'], index_col='last_update')
 
-    pot_do_datoteke1 = r'data/processed/test_prod.csv'
-    df1 = pd.read_csv(pot_do_datoteke1, parse_dates=['last_update'], index_col='last_update')
-
-    df = pd.concat([df, df1])
 
     # Izloči manjkajoče vrednosti
     print(df.isnull().sum())
@@ -76,20 +74,17 @@ with mlflow.start_run(run_name="MyModelTraining"):
 
     podatki_standardized = podatki_standardized + podatki_standardized1
 
-    # Razdelitev na učne in testne podatke
-    test_size = int(len(podatki) * 0.1)
-    train_data = podatki_standardized[:-test_size]
-    test_data = podatki_standardized[-test_size:]
+
+    train_data = podatki_standardized
+
 
     print("Oblika učnih podatkov:", train_data.shape)
-    print("Oblika testnih podatkov:", test_data.shape)
+
 
     # Priprava podatkov za model
     okno_velikost = 4
     X_train, y_train = pripravi_podatke_za_ucenje(train_data, okno_velikost)
-    X_test, y_test = pripravi_podatke_za_ucenje(test_data, okno_velikost)
     y_train = y_train.flatten()
-    y_test = y_test.flatten()
 
     # Definicija modela
     inputs = Input(shape=(X_train.shape[1], X_train.shape[2]))
@@ -118,15 +113,11 @@ with mlflow.start_run(run_name="MyModelTraining"):
     # Beleženje modela
     mlflow.keras.log_model(model_lstm, "model")
 
-    # Preverjanje uspešnosti modela
-    y_pred = model_lstm.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
 
-    # Beleženje metrik
-    mlflow.log_metrics({"mse": mse, "mae": mae, "r2": r2})
 
-    print("MSE:", mse)
-    print("MAE:", mae)
-    print("R2:", r2)
+    # Shranjevanje modela
+    model_lstm.save("models\model_lstm.h5")
+
+
+
+
